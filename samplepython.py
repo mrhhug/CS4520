b04e8562cca6e320d081d4f2fef797ebc2578475
mrhhug@gmail.com <mrhhug@gmail.com>
2013-10-14 20:17:53 -0400

    removed a file

--- samplepython.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python2
-import sys
-"""
-Python source code - this is a sample hello world, start from here. 
-"""
-print("Hello, World!")
-print(sys.version)
-# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
